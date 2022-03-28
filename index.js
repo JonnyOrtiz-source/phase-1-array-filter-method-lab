f5aeb481a7d8835c1abbad37892e9243f046d5e1
Jonny Ortiz <j0nny.0rt1z@yahoo.com>
2022-03-27 21:32:00 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution here
+// const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "Bobby"];
+
+const drivers = [
+  {
+    name: "Bobby",
+    hometown: "Pittsburgh",
+  },
+  {
+    name: "Sammy",
+    hometown: "New York",
+  },
+  {
+    name: "Sally",
+    hometown: "Cleveland",
+  },
+  {
+    name: "Annette",
+    hometown: "Los Angeles",
+  },
+  {
+    name: "Bobby",
+    hometown: "Tampa Bay",
+  },
+];
+
+function findMatching(arr, str) {
+  return arr.filter((element) => element.toUpperCase() === str.toUpperCase());
+}
+
+function fuzzyMatch(arr, str) {
+  return arr.filter((element) => element.slice(0, str.length) === str);
+}
+
+function matchName(arr, str) {
+  return arr.filter((element) => element.name === str);
+}
